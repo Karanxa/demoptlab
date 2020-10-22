976c714ca4d66161e29b0969c65890a029e06f04
Karan Arora <41389643+Karanxa@users.noreply.github.com>
2020-10-22 13:27:18 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "C"
 import "fmt"
 
-func main(){
-	f := C.intFunc(C.fortytwo)
-	fmt.Println(int(C.bridge_int_func(f)))
-
+func main() {
+    f := C.intFunc(C.fortytwo)
+    fmt.Println(int(C.bridge_int_func(f)))
+    // Output: 42
 }
